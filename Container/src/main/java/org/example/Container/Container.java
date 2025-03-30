ed2619e4c8d022d61ba3b292fbf91bfd1fbda2e4
BrightSparkle <ekaterinamrch@gmail.com>
2025-03-31 00:46:16 +0300

    Add class Container

--- Container/src/main/java/org/example/Container/Container.java
+++ Container/src/main/java/org/example/Container/Container.java
@@ -1,4 +1,50 @@
 package org.example.Container;
 
 public class Container {
+
+    private int[] elements;
+    private int size;
+
+    public Container() {
+        elements = new int[10];
+        size = 0;
+    }
+
+    public void add(int value) {
+        if (size == elements.length) {
+            increaseCapacity();
+        }
+        elements[size++] = value;
+    }
+
+    public int get(int index) {
+        checkIndex(index);
+        return elements[index];
+    }
+
+    public void remove(int index) {
+        checkIndex(index);
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = elements.length * 2;
+        int[] newArray = new int[newCapacity];
+        System.arraycopy(elements, 0, newArray, 0, size);
+        elements = newArray;
+    }
 }
